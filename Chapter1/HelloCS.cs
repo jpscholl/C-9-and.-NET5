c8e7d72807542334aa666f42e4058650c6cfdcea
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2022-03-14 23:44:53 -0400

    Create HelloCS.cs

---
+++ Chapter1/HelloCS.cs
@@ -0,0 +1,10 @@
+Console.WriteLine("Hello, C# fans!");
+Message();
+
+//main method is automatic in .NET 6, this is separate method
+
+//no longer using access modifiers (public, private, etc.)
+static void Message()
+{
+    Console.WriteLine("Most basic program ever...congrats you're a programmer now!");
+}
