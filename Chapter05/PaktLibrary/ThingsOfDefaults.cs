588a8611039e6d40286ed30f8550a5a205434998
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2022-03-24 22:06:47 -0400

    Add files via upload

---
+++ Chapter05/PaktLibrary/ThingsOfDefaults.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Pakt.Shared;
+
+public class ThingsOfDefaults
+{
+    public int Population;
+    public DateTime When;
+    public string Name;
+    public List<Person> People;
+    public ThingsOfDefaults()
+    {
+        Population = default;
+        When = default;
+        Name = default;
+        People = default;
+    }
+}
