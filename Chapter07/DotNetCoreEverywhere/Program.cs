9059c46936562b9b8193237d84bc77436df7960b
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2022-04-12 00:04:02 -0400

    Add files via upload

---
+++ Chapter07/DotNetCoreEverywhere/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 4/11/2022
+//  Project: C#9 and .NET5 - Chapter07
+//  
+*/
+
+using static System.Console;
+namespace DotNetCoreEverywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("I can run everywhere!");
+        }
+    }
+}
