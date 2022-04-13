e95d4c7688cb32e6c25e51d5b8bcdc024de6a9da
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2022-04-12 20:31:59 -0400

    Update Program.cs

--- Chapter08/WorkingWithDictionaries/Program.cs
+++ Chapter08/WorkingWithDictionaries/Program.cs
@@ -1,1 +1,30 @@
+/*
+//  Author: Jonathan Scholl
+//  Date: 4/12/2022
+//  Project: C#9 and .NET5 - Chapter08
+//  
+*/
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
+namespace WorkingWithDictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var keywords = new Dictionary<string, string>();
+            keywords.Add("int", "32-bit integer data type");
+            keywords.Add("long", "64-bit integer data type");
+            keywords.Add("float", "Single precision floating point number");
+            Console.WriteLine("Keywords and their definitions");
+            foreach (KeyValuePair<string, string> item in keywords)
+            {
+                Console.WriteLine($" {item.Key}: {item.Value}");
+            }
+            Console.WriteLine($"The definition of long is {keywords["long"]}");
+
+        }
+    }
+}
