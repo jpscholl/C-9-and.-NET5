946842814a6ec0e201148e1a81149ff2dc6ca053
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2022-04-20 20:33:01 -0400

    Add files via upload

---
+++ Chapter09/WorkingWithSerialization/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 4/20/2022
+//  Project: C#9 and .NET5 - Chapter09 - WorkingWithSerialization
+//  
+*/
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using Packt.Shared;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+namespace WorkingWithSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
