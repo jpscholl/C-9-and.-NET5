0fd26f87f9c8f58fd86eda07e12b316eb74ad7b2
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2023-10-30 18:44:44 -0400

    Add files via upload

---
+++ Chapter10/SigningApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Packt.Shared;
+
+namespace SigningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some text to sign:");
+            string data = Console.ReadLine();
+            var signature = Protector.GenerateSignature(data);
+            Console.WriteLine($"Signature: {signature}");
+            Console.WriteLine("Public key used to check signature:");
+            Console.WriteLine(Protector.PublicKey);
+
+            if (Protector.ValidateSignature(data, signature))
+            {
+                Console.WriteLine("Correct! Signature is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid signature");
+            }
+            //simulate a fake signature by replacing the first character with an X
+            var fakeSignature = signature.Replace(signature[0], 'X');
+            if(Protector.ValidateSignature(data, fakeSignature))
+            {
+                Console.WriteLine("Correct! Signature is valid.");
+            }
+            else
+            {
+                Console.WriteLine($"Invalid signature: {fakeSignature}");
+            }
+        }
+    }
+}
