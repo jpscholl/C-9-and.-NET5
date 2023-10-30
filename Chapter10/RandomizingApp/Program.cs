1f3657653589d8ee0c259fff76a392963b1b3ee0
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2023-10-30 18:43:37 -0400

    Add files via upload

---
+++ Chapter10/RandomizingApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Packt.Shared;
+
+namespace RandomizingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How big do you want the key (in bytes): ");
+            string size = Console.ReadLine();
+            byte[]key = Protector.GetRandomKeyOrIV(int.Parse(size));
+            Console.WriteLine($"\nKey as byte array:");
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.Write($"{key[i]:x2} ");
+                if (((i + 1) % 16) == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
