c876f314b152b5c6829483a6d8e244ea06ba1a21
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2023-10-30 18:42:13 -0400

    Add files via upload

---
+++ Chapter10/EncryptionApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using Packt.Shared;
+
+namespace EncryptionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a message that you want to encrypt: ");
+            string message = Console.ReadLine();
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+            string cryptoText = Protector.Encrypt(message, password);
+            Console.WriteLine($"Encrypted text: {cryptoText}");
+            Console.Write("Enter the password:");
+            string password2 = Console.ReadLine();
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password2);
+                Console.WriteLine($"Decrypted text:{clearText}");
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine("{0}\nMore details: {1}",
+                    arg0: "You entered the wrong password!",
+                    arg1: ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Non-cryptogaphic exception:{0}, {1}",
+                    arg0: ex.GetType().Name,
+                    arg1: ex.Message);
+            }
+        }
+    }
+}
